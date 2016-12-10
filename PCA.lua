09febfb7ed96f4ec51b50ad88c76fed1c36aeb1d
Sagar M. Waghmare <SW@discoverelement.com>
2016-12-10 14:28:29 -0500

    PCA:project()

--- PCA.lua
+++ PCA.lua
@@ -6,8 +6,9 @@
 local PCA, parent = torch.class('unsupervised.PCA',
                                 'unsupervised.ParentModule')
 
-function PCA:__init()
+function PCA:__init(M)
    self.trained = false
+   self.M = M
 end
 
 function PCA:_meanZero(X, mean, inplace)
@@ -31,10 +32,9 @@ function PCA:_meanZero(X, mean, inplace)
 end
 
 -- data is NxD tensor.
-function PCA:train(X, M, inplace)
+function PCA:train(X, inplace)
    self.N = X:size(1)
    self.D = X:size(2)
-   self.M = M or self.D
    local inplace = inplace or false
 
    -- Compute mean
@@ -48,28 +48,41 @@ function PCA:train(X, M, inplace)
    self.covar:div(self.N - 1)
 
    -- Eigen value decomposition for Symmetric matrix
+   -- Eigenvectors are transposed here hence each row is a eigen vector
    local values, vectors = torch.symeig(self.covar, 'V')
 
    --[[ Output returned by symeig are in ascending order or eigen values
         hence reversing the order --]]
-   local indices = torch.linspace(self.D, 1, self.D):long()
-   self.values = values.new()
+   local indices
+   self.values, indices = values:sort(true)
    self.vectors = vectors.new()
-   self.values:index(values, 1, indices)
    self.vectors:index(vectors, 1, indices)
    self.trained = true
 end
 
 -- Project to PCA dimensions
-function PCA:project(input, meanZeroed, inplace)
-
+function PCA:project(X, M, meanZeroed, inplace)
+   assert(self.trained, "PCA not trained.")
    local meanZeroed = meanZeroed or false
    local inplace = inplace or false
+   M = self.M or M
 
-   if inplace then
+   assert(M<=self.D, "M should be less than or equal to self.D.")
+
+   -- Mean zero X
+   local normedX = X.new()
+   if meanZeroed then
       normedX = X
    else
-      normedX:resizeAs(X):copy(X)
+      normedX = self:_meanZero(X, self.mean, inplace)
    end
 
+   -- Pick first M eigen vectors
+   local V = self.vectors[{{1, M}}]:t()
+
+   -- Original Equation is E_t(M, D) * normedX(D,N) = (M, N)
+   -- We do normedX(N,D) * V_t(D,M) = (N,M)
+   -- V = E_t
+   local Y = torch.mm(normedX, V)
+   return Y
 end
