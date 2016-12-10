8f6ccf7837ccaa0211e5718eb85eb56c8e97aa65
Sagar M. Waghmare <SW@discoverelement.com>
2016-12-10 15:12:44 -0500

    PCA unit test.

--- test.lua
+++ test.lua
@@ -15,13 +15,23 @@ function unitTests.PCA()
    local M = 2
    local data = torch.rand(N, D)
 
-   local pca = unsupervised.PCA()
+   local pca = unsupervised.PCA(M)
    pca:train(data)
 
+   -- Mean must match
    local mean = data:mean(1)
-
    mytester:assertTensorEq(mean, pca.mean, precision,
                            "Incorrect Mean vectors.")
+
+   -- Eigen vectors should be unit vectors
+   local norms = pca.vectors:norm(2, 2)
+   mytester:assertTensorEq(norms, torch.ones(D, 1), precision,
+                           "Eigen vectors don't have unit length.")
+  
+   local Y = pca:project(data)
+   local yMean = Y:mean(1)
+   mytester:assertTensorEq(yMean, torch.zeros(1, M), precision,
+         "Projected vectors Mean should be very close to zero.")
 end
 
 function unsupervised.test(tests)
