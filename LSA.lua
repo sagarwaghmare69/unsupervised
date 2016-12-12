70e60ae9c9178149945a7511ac30513b71f0a843
Sagar M. Waghmare <SW@discoverelement.com>
2016-12-12 09:32:58 -0500

    LSA:train() and LSA:project()

--- LSA.lua
+++ LSA.lua
@@ -15,6 +15,61 @@ function LSA:__init(M, rescale)
    self.rescale = rescale or true -- Default rescale projections
 end
 
-function LSA:__init(X, inplace)
+function LSA:train(X, inplace)
+   self.N = X:size(1)
+   self.D = X:size(2)
+   local inplace = inplace or false
 
+   -- Compute mean
+   self.mean = X:mean(1)
+
+   -- Mean zero the X
+   local normedX = self:_meanZero(X, self.mean, inplace)
+
+   -- SVD
+   local U, S, V = torch.svd(normedX:t())
+
+   -- Eigen values and eigen vectors
+   self.Dvalues = S
+   self.vectors = U
+   -- Eigen Values = square of singular values
+   self.values = self.Dvalues:clone()
+   self.values:pow(2)
+
+   --[[ SVD output is in descending order of eigen values hence
+        no need to sort eigen values and eigen vectors --]]
+   self.trained = true
+end
+
+-- Project to M dimensions
+function LSA:project(X, meanZeroed, inplace, M)
+   assert(self.trained, "LSA not trained.")
+   local meanZeroed = meanZeroed or false
+   local inplace = inplace or false
+   local M = self.M or M
+
+   assert(M<=self.D, "M should be less than or equal to self.D.")
+
+   -- Mean zero X
+   local normedX = X.new()
+   if meanZeroed then
+      normedX = X
+   else
+      normedX = self:_meanZero(X, self.mean, inplace)
+   end
+
+   local U = self.vectors[{{1, M}}]:t()
+   local Dvalues = self.Dvalues[{{1, M}}]
+   local DinvValues = Dvalues:clone():fill(1):cdiv(Dvalues)
+   local Dinv = torch.diag(DinvValues)
+
+   local tempY = torch.mm(normedX, U)
+   local Y = tempY.new()
+   if self.rescale then
+      Y = torch.mm(tempY, Dinv)
+      Y:mul(math.sqrt(self.N-1))
+   else
+      Y = tempY
+   end
+   return Y
 end
